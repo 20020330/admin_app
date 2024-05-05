05dab93b9cdffc7c5ce0a9cfb7e515fdc0b4f2fc
truonggiangggg <nhoxtruong2002@gmail.com>
2024-05-06 00:07:21 +0700

    add scr

--- index.php
+++ index.php
@@ -37,8 +37,17 @@ include_once (__DIR__ .'\\db\\connection.php');
   <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
   <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
+<script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
 </script>
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="./assets/js/soft-ui-dashboard.min.js?v=1.0.7"></script>
+</html>
