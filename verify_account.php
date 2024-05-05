e72df94a4e98f45f0d71e5d035fe466e9177a486
truonggiangggg <nhoxtruong2002@gmail.com>
2024-05-05 22:55:46 +0700

    add footer rspass

--- verify_account.php
+++ verify_account.php
@@ -70,3 +70,18 @@
             </div>
         </section>
     </main>
+ <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+ <footer class="footer py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-8 mx-auto text-center mt-1">
+                    <p class="mb-0 text-secondary">
+                        Copyright © <script>
+                            document.write(new Date().getFullYear())
+                        </script> TranVux.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->    
