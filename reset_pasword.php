2c5a24e885bb9703466b57ba2dd7f8be6854bd18
truonggiangggg <nhoxtruong2002@gmail.com>
2024-05-05 22:51:43 +0700

    up footer

--- reset_pasword.php
+++ reset_pasword.php
@@ -23,3 +23,68 @@
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
 </head>
+
+<body class="">
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+    <main class="main-content  mt-0">
+        <section>
+            <div class="page-header min-vh-75">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+                            <div class="card card-plain mt-8">
+                                <div class="card-header pb-0 text-left bg-transparent">
+                                    <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
+                                    <p class="mb-0">Nhập mật khẩu mới của bạn</p>
+                                </div>
+                                <div class="card-body">
+                                    <form role="form" id="form" method="post">
+                                        <label>Mật khẩu mới</label>
+                                        <div class="mb-3">
+                                            <input type="password" class="form-control" placeholder="New Password" name="new_password" aria-label="Email" aria-describedby="email-addon">
+                                        </div>
+                                        <label>Nhập lại mật khẩu mới</label>
+                                        <div class="mb-3">
+                                            <input type="password" class="form-control" placeholder="ReNew Password" name="confirm_password" aria-label="Password" aria-describedby="password-addon">
+                                            <input hidden class="form-control" placeholder="ReNew Password" name="email" aria-label="Password" aria-describedby="password-addon" value="<?php echo $_GET["email"] ?>">
+                                            <input hidden class="form-control" placeholder="ReNew Password" name="token" aria-label="Password" aria-describedby="password-addon" value="<?php echo $_GET["token"] ?>">
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" name="btn_submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Đổi mật khẩu</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="oblique position-absolute top-0 h-100 d-md-block d-none me-n8">
+                                <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('./assets/img/curved-images/curved6.jpg')"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+    <footer class="footer py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-8 mx-auto text-center mt-1">
+                    <p class="mb-0 text-secondary">
+                        Copyright © <script>
+                            document.write(new Date().getFullYear())
+                        </script> TranVux.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
+
