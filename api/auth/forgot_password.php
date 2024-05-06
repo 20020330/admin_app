4f64ccc06652faa5d66fd1143923e7bb98864c6c
truonggiangggg <nhoxtruong2002@gmail.com>
2024-05-06 17:52:22 +0700

    must

--- api/auth/forgot_password.php
+++ api/auth/forgot_password.php
@@ -28,4 +28,40 @@ try {
     $token = md5(time() . $email);
     //luu token vao database
     $dbConn->query("insert into reset_password(id_user, email,token) values('$userId', '$email','$token')");
+    $link = "http://127.0.0.1:3000/reset_password.php?email="
+        . $email . "&token=$token";
+    $mail = new PHPMailer();
+    $mail->CharSet = "utf-8";
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    $mail->Username = "vutran789jjjj";
+    $mail->Password = "byaobfidnumjksdk";
+    $mail->SMTPSecure = "ssl";
+    $mail->Host = "ssl://smtp.gmail.com";
+    $mail->Port = "465";
+    $mail->From = "vutran789jjjj@gmail.com";
+    $mail->FromName = "TranVux";
+    $mail->addAddress($email, 'Hello');
+    $mail->Subject = "Reset Password";
+    $mail->isHTML(true);
+    // $mail->Body = "Click on this link to reset password " . $link . " ";
+    $mail->Body = getTempMail($link, "Thay đổi mật khẩu");
+    $res = $mail->Send();
 
+    if ($res) {
+        echo json_encode(array(
+            "status" => 200,
+            "message" => "Email send."
+        ));
+    } else {
+        echo json_encode(array(
+            "status" => 400,
+            "message" => "Email send failed."
+        ));
+    }
+} catch (Exception $e) {
+    echo json_encode(array(
+        "status" => 400,
+        "message" => $e->getMessage()
+    ));
+}
