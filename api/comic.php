8ae5976f038789cee1c85ffc2bd55c3dff1ec764
truonggiangggg <nhoxtruong2002@gmail.com>
2024-05-06 17:18:55 +0700

    api comic

--- api/comic.php
+++ api/comic.php
@@ -1,2 +1,23 @@
 <?php
 include_once("../db/connection.php");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+try {
+    $comics = $dbConn->query("SELECT cm.title, cm.synopsis, cm.sub_title, cm.id, cm.thumbnail, group_concat(CONCAT(' ',cm_cate.title_category)) AS `categories`, au.name, cm.country, us.user_name
+    FROM `comic_category` as cm_cate inner join comic as cm on cm_cate.id_comic = cm.id 
+                                     inner join author as au on cm.author = au.id
+                                     inner join user as us on cm.publish_by = us.id
+    GROUP BY cm_cate.id_comic")->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode(array(
+        "status" => "success",
+        "result" => $comics,
+    ));
+} catch (Exception $e) {
+    echo json_encode(array(
+        "status" => "error",
+        "message" => $e->getMessage()
+    ));
+}
