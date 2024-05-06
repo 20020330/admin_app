17bf22e9474053838d765e34aae329e653bd7446
truonggiangggg <nhoxtruong2002@gmail.com>
2024-05-06 17:48:19 +0700

    must

--- api/auth/forgot_password.php
+++ api/auth/forgot_password.php
@@ -13,3 +13,19 @@ use PHPMailer\PHPMailer\PHPMailer;
 include_once '../../utilities/PHPMailer-master/src/PHPMailer.php';
 include_once '../../utilities/PHPMailer-master/src/SMTP.php';
 include_once '../../utilities/PHPMailer-master/src/Exception.php';
+try {
+    // lay mail tu body
+    $data = json_decode(file_get_contents("php://input"));
+    $email = $data->email;
+    //kt mail co ton tai ko
+    $result = $dbConn->query("select id from user where email like '$email'");
+    $user = $result->fetch(PDO::FETCH_ASSOC);
+    $userId = $user["id"];
+    if (!$user) {
+        throw new Exception("Email khong ton tai");
+    }
+    // tao token
+    $token = md5(time() . $email);
+    //luu token vao database
+    $dbConn->query("insert into reset_password(id_user, email,token) values('$userId', '$email','$token')");
+
